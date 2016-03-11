a2685598ba2a8048754d07a8265aa7d1d65ab3b7
Fernando Nunes <jaruche@gmail.com>
2016-03-11 14:27:27 -0500

    Initial

---
+++ post_push.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ssh pi@dex.local cd Jaruchinho | chmod +x update.sh | ./update.sh
--- update.sh
+++ update.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
-DEPLOYDIR=~/apps/testing_app
 
-GIT_WORK_TREE="$DEPLOYDIR" git checkout -f
-
-cd "$DEPLOYDIR"
-
-forever stop index.js
-
-npm install
-
-forever start index.js
-
-echo "All done ;)"
+git pull
