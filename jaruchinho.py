a0658065ce53c9f7fe046be8267393323e089347
Fernando Nunes <jaruche@gmail.com>
2016-03-11 15:16:05 -0500

    Initial

--- jaruchinho.py
+++ jaruchinho.py
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
 from ps3 import *		#Import the PS3 library
 from gopigo import *	#Import the GoPiGo library
 
-print "Initializing"
+print "Initialasdadsizing"
 p=ps3()		#Create a PS3 object
 print "Done"
 s=80	#Initialize
