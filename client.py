4f7781dcb9ba181d71ab9c1ade00fee5828dc5ef
Fernando Nunes <jaruche@gmail.com>
2016-03-13 03:05:19 -0400

    Initial

--- client.py
+++ client.py
@@ -15,21 +15,10 @@ try:
         camera.vflip = True
         camera.hflip = True
         camera.resolution = (320, 240)
-        camera.framerate = 24
-        camera.annotate_background = picamera.Color('black')
-        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        camera.framerate = 10
+        # camera.annotate_text = "Teste"
         camera.start_recording(connection, format='h264')
-        start = dt.datetime.now()
-        while (dt.datetime.now() - start).seconds < 30:
-            camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            camera.wait_recording(0.2)
-        # Start a preview and let the camera warm up for 2 seconds
-        # camera.start_preview()
-        # time.sleep(2)
-        # Start recording, sending the output to the connection for 60
-        # seconds, then stop
-        # camera.wait_recording(60)
-        # camera.stop_recording()
+
         input("Recording...")
 finally:
     connection.close()
