542e3bb2d275cbf1ba19f3dfa2781b064d95a557
Fernando Nunes <jaruche@gmail.com>
2016-03-13 01:33:23 -0500

    Initial

--- client.py
+++ client.py
@@ -11,8 +11,10 @@ client_socket.connect(('FernandoiMac.local', 8000))
 connection = client_socket.makefile('wb')
 try:
     with picamera.PiCamera() as camera:
-        camera.resolution = (640, 480)
-        camera.framerate = 24
+        camera.vflip = True
+        camera.hflip = True
+        camera.resolution = (320, 240)
+        camera.framerate = 10
         # Start a preview and let the camera warm up for 2 seconds
         camera.start_preview()
         time.sleep(2)
--- server.py
+++ server.py
@@ -12,13 +12,13 @@ connection = server_socket.accept()[0].makefile('rb')
 try:
     # Run a viewer with an appropriate command line. Uncomment the mplayer
     # version if you would prefer to use mplayer instead of VLC
-    cmdline = ['vlc', '--demux', 'h264', '-']
+    cmdline = ['/Applications/VLC.app/Contents/MacOS/VLC', '--demux', 'h264', '-']
     #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
     player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
     while True:
         # Repeatedly read 1k of data from the connection and write it to
         # the media player's stdin
-        data = connection.read(1024)
+        data = connection.read(512)
         if not data:
             break
         player.stdin.write(data)
