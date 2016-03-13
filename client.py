95f6aec4409259912f0d1fc63c57e00abe885899
Fernando Nunes <jaruche@gmail.com>
2016-03-13 03:01:12 -0400

    Initial

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import picamera
+import datetime as dt
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
@@ -15,7 +16,12 @@ try:
         camera.hflip = True
         camera.resolution = (320, 240)
         camera.framerate = 24
-        camera.annotate_text = 'Hello world!'
+        camera.annotate_background = picamera.Color('black')
+        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        start = dt.datetime.now()
+        while (dt.datetime.now() - start).seconds < 30:
+            camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            camera.wait_recording(0.2)
         # Start a preview and let the camera warm up for 2 seconds
         # camera.start_preview()
         # time.sleep(2)
