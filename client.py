d43e13658ef29969e230443281beeaf5b5c89140
Fernando Nunes <jaruche@gmail.com>
2016-03-13 01:38:53 -0500

    Initial

--- client.py
+++ client.py
@@ -13,16 +13,16 @@ try:
     with picamera.PiCamera() as camera:
         camera.vflip = True
         camera.hflip = True
-        camera.resolution = (320, 240)
+        camera.resolution = (640, 480)
         camera.framerate = 10
         # Start a preview and let the camera warm up for 2 seconds
-        camera.start_preview()
-        time.sleep(2)
+        # camera.start_preview()
+        # time.sleep(2)
         # Start recording, sending the output to the connection for 60
         # seconds, then stop
         camera.start_recording(connection, format='h264')
-        camera.wait_recording(60)
-        camera.stop_recording()
+        # camera.wait_recording(60)
+        # camera.stop_recording()
 finally:
     connection.close()
     client_socket.close()
