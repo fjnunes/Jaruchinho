fbafefcb89755cdaaf956b0724b54c3d54b0f516
jaruche <jaruche@gmail.com>
2016-03-26 22:34:08 -0400

    Initial

--- client.py
+++ client.py
@@ -20,70 +20,70 @@ enable_encoders()
 enc_tgt(1,1,1)
 fwd()
 
-try:
-    camera = picamera.PiCamera()
-    # camera.vflip = True
-    # camera.hflip = True
-    camera.resolution = (640, 480)
-    camera.framerate = 4
+# try:
+camera = picamera.PiCamera()
+# camera.vflip = True
+# camera.hflip = True
+camera.resolution = (640, 480)
+camera.framerate = 4
 
-    while True:
-        command = ''
-        distance = us_dist(15)
-        p.update()  # Read the ps3 values
+while True:
+    command = ''
+    distance = us_dist(15)
+    p.update()  # Read the ps3 values
 
-        if p.up:  # If UP is pressed move forward
-            command = 'f'
-        elif p.left:  # If LEFT is pressed turn left
-            command = 'l'
-        elif p.right:  # If RIGHT is pressed move right
-            command = 'r'
-        elif p.down:  # If DOWN is pressed go back
-            command = 'b'
-        else:
-            command = 's'
+    if p.up:  # If UP is pressed move forward
+        command = 'f'
+    elif p.left:  # If LEFT is pressed turn left
+        command = 'l'
+    elif p.right:  # If RIGHT is pressed move right
+        command = 'r'
+    elif p.down:  # If DOWN is pressed go back
+        command = 'b'
+    else:
+        command = 's'
 
-        if p.circle:
-            led_on(LED_L)
-            led_on(LED_R)
-        else:
-            # stop()
-            led_off(LED_L)
-            led_off(LED_R)
+    if p.circle:
+        led_on(LED_L)
+        led_on(LED_R)
+    else:
+        # stop()
+        led_off(LED_L)
+        led_off(LED_R)
 
-        if p.triangle:
-            print "Distance: "+ distance
+    if p.triangle:
+        print "Distance: "+ distance
 
-        if p.l2:
-            if not connected:
-                camera_socket = socket.socket()
-                camera_socket.connect(('FernandoMacBookPro.local', 8000))
-                camera_connection = camera_socket.makefile('wb')
-                jaruchinho_connection = jaruchinho_socket.accept()[0].makefile('rb')
-                connected = True
-            camera.start_recording(camera_connection, format='mjpeg')
-            command = jaruchinho_connection.read(1)
-        else:
-            camera.stop_recording()
+    if p.l2:
+        if not connected:
+            camera_socket = socket.socket()
+            camera_socket.connect(('FernandoMacBookPro.local', 8000))
+            camera_connection = camera_socket.makefile('wb')
+            jaruchinho_connection = jaruchinho_socket.accept()[0].makefile('rb')
+            connected = True
+        camera.start_recording(camera_connection, format='mjpeg')
+        command = jaruchinho_connection.read(1)
+    else:
+        camera.stop_recording()
 
-        if distance <= 5:
-            command = 's'
+    if distance <= 5:
+        command = 's'
 
-        print(command)
-        if command == 'f':
-            fwd()
-        elif command == 'r':
-            right()
-        elif command == 'l':
-            left()
-        elif command == 'b':
-            bwd()
-        elif command == 's':
-            stop()
-        else:
-            stop()
-            time.sleep(1)
+    print(command)
+    if command == 'f':
+        fwd()
+    elif command == 'r':
+        right()
+    elif command == 'l':
+        left()
+    elif command == 'b':
+        bwd()
+    elif command == 's':
+        stop()
+    else:
+        stop()
+        time.sleep(1)
 
-finally:
-    camera_connection.close()
-    camera_socket.close()
+# finally:
+#     camera_connection.close()
+#     camera_socket.close()
