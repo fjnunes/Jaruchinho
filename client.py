30e8a554eccc3910e15fca0aebb19b04bd83071f
jaruche <jaruche@gmail.com>
2016-03-26 22:17:41 -0400

    Initial

--- client.py
+++ client.py
@@ -18,31 +18,72 @@ camera_connection = camera_socket.makefile('wb')
 
 set_speed(50)
 
+p = ps3()  # Create a PS3 object
+s = 80  # Initializ
+
+# Little step to indicate that the script has started
+enable_encoders()
+enc_tgt(1,1,1)
+fwd()
+
 try:
-    with picamera.PiCamera() as camera:
-        camera.vflip = True
-        camera.hflip = True
-        camera.resolution = (640, 480)
-        camera.framerate = 4
-        # camera.annotate_text = "Teste"
-        camera.start_recording(camera_connection, format='mjpeg')
+    camera = picamera.PiCamera()
+    # camera.vflip = True
+    # camera.hflip = True
+    camera.resolution = (640, 480)
+    camera.framerate = 4
+    camera.start_recording(camera_connection, format='mjpeg')
 
-        if not connected:
-            jaruchinho_connection = jaruchinho_socket.accept()[0].makefile('rb')
-            connected = True
+    while True:
+        command = ''
+        distance = us_dist(15)
+        p.update()  # Read the ps3 values
 
-        while True:
+        if p.up:  # If UP is pressed move forward
+            command = 'f'
+        elif p.left:  # If LEFT is pressed turn left
+            command = 'l'
+        elif p.right:  # If RIGHT is pressed move right
+            command = 'r'
+        elif p.down:  # If DOWN is pressed go back
+            command = 'b'
+        else:
+            command = 's'
+
+        if p.circle:
+            led_on(LED_L)
+            led_on(LED_R)
+        else:
+            # stop()
+            led_off(LED_L)
+            led_off(LED_R)
+
+        if p.triangle:
+            print "Distance: "+ distance
+
+        if p.l2:
+            if not connected:
+                jaruchinho_connection = jaruchinho_socket.accept()[0].makefile('rb')
+                connected = True
             command = jaruchinho_connection.read(1)
-            print(command)
-            if command == 'f':
-                fwd()
-            elif command == 'r':
-                right()
-            elif command == 'l':
-                left()
-            else:
-                stop()
-                time.sleep(1)
+
+        if distance <= 5:
+            command = 's'
+
+        print(command)
+        if command == 'f':
+            fwd()
+        elif command == 'r':
+            right()
+        elif command == 'l':
+            left()
+        elif command == 'b':
+            bwd()
+        elif command == 's':
+            stop()
+        else:
+            stop()
+            time.sleep(1)
 
 finally:
     camera_connection.close()
