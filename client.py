2364464af11c2fb6a9063928f04907a97d76fe39
jaruche <jaruche@gmail.com>
2016-03-28 00:45:05 -0400

    Initial

--- client.py
+++ client.py
@@ -28,7 +28,7 @@ camera = picamera.PiCamera()
 # camera.vflip = True
 # camera.hflip = True
 camera.resolution = (320, 240)
-camera.framerate = 30
+camera.framerate = 12
 
 while True:
     command = ''
