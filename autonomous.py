218cbea96d81b12eb7f6ecde4eb29551f5d004c8
jaruche <jaruche@gmail.com>
2016-04-06 16:55:59 -0400

    Initial

--- autonomous.py
+++ autonomous.py
@@ -59,7 +59,7 @@ print "Warming up camera"
 time.sleep(2)
 
 print "Starting the motors"
-set_speed(80)
+set_speed(60)
 # enable_com_timeout(1000)
 # Little step to indicate that the script has started
 enable_encoders()
