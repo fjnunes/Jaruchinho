9040ad1ac738f80db56e3a4a2de7f9bb31f3dc61
Fernando Nunes <jaruche@gmail.com>
2016-04-05 20:55:18 -0400

    Initial

--- autonomous.py
+++ autonomous.py
@@ -38,16 +38,17 @@ while True:
 
     start = time.time()
     stream = io.BytesIO()
-    camera.capture(stream, format='png', resize=(160, 120), use_video_port=True) # change to 'yuv' later
+    # camera.capture(stream, format='png', resize=(160, 120), use_video_port=True) # change to 'yuv' later
+    time.sleep(0.1)
     done = time.time()
     camera_elapsed = done - start
 
     start = time.time()
-    stream.seek(0)
-    image = Image.open(stream)
-    image = image.convert('L') #makes it greyscale
-    image_data = numpy.array(image)
-    image_data = image_data.reshape(1, 19200)
+    # stream.seek(0)
+    # image = Image.open(stream)
+    # image = image.convert('L') #makes it greyscale
+    # image_data = numpy.array(image)
+    # image_data = image_data.reshape(1, 19200)
     done = time.time()
     image_elapsed = done - start
 
