7a624999e3b3419faaecd9a968dcc5124ee35b04
Fernando Nunes <jaruche@gmail.com>
2016-05-08 15:56:38 -0400

    Initial

--- Arduino2Pi.py
+++ Arduino2Pi.py
@@ -5,5 +5,5 @@ ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 # ser = serial.Serial('/dev/tty.usbmodemFD141', 115200, timeout=1)
 while True:
     ser.write("s1200\n")
-    ser.write("t1600\n")
+    ser.write("t1650\n")
     time.sleep(0.05)
