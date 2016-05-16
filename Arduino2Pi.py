02b381ab3a89d487f058558a2f84b79758a6994c
Fernando Nunes <jaruche@gmail.com>
2016-05-16 17:10:30 -0400

    Initial

--- Arduino2Pi.py
+++ Arduino2Pi.py
@@ -4,6 +4,17 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 # ser = serial.Serial('/dev/tty.usbmodemFD141', 115200, timeout=1)
 while True:
-    ser.write("s1200\n")
-    ser.write("t1650\n")
-    time.sleep(0.05)
+    servoVal = "s1200\r\n"
+    throtleVal = "t1650\r\n"
+
+    ser.write(servoVal)
+    servo = ser.readline()
+    if (servo != servoVal):
+        print "Took over servo"
+
+    # ser.write(throtleVal)
+    # throtle = ser.readline()
+    # if (servo != servoVal):
+    #     print "Took over throtle"
+
+    time.sleep(0.1)
