35d62ec25215aad3e5917c6915d8142077bcc67c
Fernando Nunes <jaruche@gmail.com>
2016-05-17 22:37:48 -0400

    Initial

--- Arduino2Pi.py
+++ Arduino2Pi.py
@@ -5,15 +5,12 @@ ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 # ser = serial.Serial('/dev/tty.usbmodemFD141', 115200, timeout=1)
 while True:
 
-    tookover = False
     ser.write("s1200\n")
-    response = ser.readline()
-    if response != "OK\r\n":
-        tookover = True
+    steering = ser.readline()
 
     ser.write("t1650\n")
-    response = ser.readline()
-    if response != "OK\r\n" and response != '' and int(response) > 1486+20:
+    throttle = ser.readline()
+    if throttle != "OK\r\n" and throttle != '' and int(throttle) > 1486+20:
         print "Took over"
 
     time.sleep(0.1)
